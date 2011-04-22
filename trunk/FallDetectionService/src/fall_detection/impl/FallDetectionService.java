29f8b5cf3d587ea03a1040df491c9e8614f6c515
Ryan Tew <rmtew2@gmail.com>
2011-04-22 09:20:32 -0500

    Added sensor change listeners and implemented what they do.
    Signed-off-by: Ryan Tew <rmtew2@gmail.com>

--- trunk/FallDetectionService/src/fall_detection/impl/FallDetectionService.java
+++ trunk/FallDetectionService/src/fall_detection/impl/FallDetectionService.java
@@ -12,9 +12,18 @@ import fall_detection.FallDetection;
 
 public class FallDetectionService implements FallDetection
 {
-	private static final int INTERFACE_KIT_SERIAL_NUM = 76397;
+	private static final double FORCE_TO_CANCEL = 10;
+	
+	private static final int NUM_INTERFACE_KIT_PORTS = 8;
+	
+	private static final int INTERFACE_KIT_SERIAL_NUM_0 = 76397;
+	
+	private static final int SENSOR_GRID_WIDTH = 3;
+	private static final int SENSOR_GRID_HEIGHT = 3;
 	
 	private static final double SENSOR_CONVERSION_FACTOR = 25.71;
+	
+	private double[][] sensorArray;
 	
 	//test method
 	public static void main(String args[]) throws PhidgetException
@@ -70,7 +79,7 @@ public class FallDetectionService implements FallDetection
 			}
 		});
 
-        ifk.open(INTERFACE_KIT_SERIAL_NUM);
+        ifk.open(INTERFACE_KIT_SERIAL_NUM_0);
         
         try {
 			Thread.sleep(30000);
@@ -82,7 +91,14 @@ public class FallDetectionService implements FallDetection
 	
 	public FallDetectionService() throws PhidgetException
 	{
-		InterfaceKitPhidget ifk = new InterfaceKitPhidget();
+		sensorArray = new double[SENSOR_GRID_HEIGHT][SENSOR_GRID_WIDTH];
+		
+		InterfaceKitPhidget[] ifk = new InterfaceKitPhidget[1];
+		
+		ifk[0] = new InterfaceKitPhidget();
+		ifk[0].addSensorChangeListener(new PressureSensorListener(0));
+		ifk[0].addSensorChangeListener(new CancelButtonListener());
+		ifk[0].open(INTERFACE_KIT_SERIAL_NUM_0);
 	}
 	
 	@Override
@@ -90,5 +106,45 @@ public class FallDetectionService implements FallDetection
 	{
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private void detectFall()
+	{
+		//TODO fall detection
+		
+		//TODO if fall is detected sound alarm
+	}
+
+	private class PressureSensorListener implements SensorChangeListener
+	{
+		private int startingIndex;
+		
+		public PressureSensorListener(int deviceIndex)
+		{
+			this.startingIndex = deviceIndex * FallDetectionService.NUM_INTERFACE_KIT_PORTS - 1;
+		}
+
+		@Override
+		public void sensorChanged(SensorChangeEvent e)
+		{
+			int i = e.getIndex() + startingIndex;
+			int r = i / FallDetectionService.SENSOR_GRID_WIDTH;
+			int c = i % FallDetectionService.SENSOR_GRID_WIDTH;
+			
+			FallDetectionService.this.sensorArray[r][c] = e.getValue() / SENSOR_CONVERSION_FACTOR;
+			FallDetectionService.this.detectFall();
+		}
+	}
+	
+	private class CancelButtonListener implements SensorChangeListener
+	{
+		@Override
+		public void sensorChanged(SensorChangeEvent e)
+		{
+			if(e.getValue() / SENSOR_CONVERSION_FACTOR >= FallDetectionService.FORCE_TO_CANCEL)
+			{
+				//TODO cancel alarm
+			}
+		}
 	}
 }
